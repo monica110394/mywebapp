66cb3b095f84753d081a83d92df3a312a863659d
monica110394 <mmon0012@student.monash.edu>
2020-05-18 03:29:40 +1000

    fg

--- static/js/home.js
+++ static/js/home.js
@@ -3,3 +3,23 @@ $(document).ready(function(){
         location.reload(true);
     })
 });
+
+function password() {
+        var testV = 1;
+        var pass1 = prompt('Please enter the password', '');
+        while (testV < 3) {
+             if (!pass1)
+                 history.go(-1);
+             if (pass1 == "flora") {
+                 alert('Welcome to ScrubShrubs!');
+                 break;
+             }
+             testV += 1;
+             var pass1 =
+                 prompt('Password error, please retype!');
+         }
+         if (pass1 != "password" & testV == 3)
+             history.go(-1);
+         return " ";
+     }
+    document.write(password());
